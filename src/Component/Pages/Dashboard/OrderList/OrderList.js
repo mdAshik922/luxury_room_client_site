3b4cb07ca323d54adb0c06e6b9884843d092ee8f
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-21 18:43:40 +0600

    commit

--- src/Component/Pages/Dashboard/OrderList/OrderList.js
+++ src/Component/Pages/Dashboard/OrderList/OrderList.js
@@ -57,7 +57,7 @@ const OrderList = () => {
           })
             .then((res) => res.json())
             .then((data) => {
-              if (data.deletedCount) {
+              if (data.deletedCount > 0) {
                 const modifiedOrders = orders.filter((order) => order._id !== id);
                 setOrders(modifiedOrders);
                 Swal.fire("Deleted!", "", "success");
