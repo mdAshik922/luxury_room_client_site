02646bede8b6da46afc90d58fa83d1ca9d7b79ae
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-22 16:18:53 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
 
             <Route path="*" element={<NotFound/>} />
           </Routes>
-          <Footer></Footer>
+          
         </BrowserRouter>
       </AuthProvider>
     </div>
