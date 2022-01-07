bcd1dddde774c74918e445a494440575811c900e
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-07 09:01:16 +0600

    commit

--- src/Component/Pages/Dashboard/Payment/Payment.js
+++ src/Component/Pages/Dashboard/Payment/Payment.js
@@ -59,7 +59,7 @@ const Payment = () => {
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">New message</h5>
+        <h5 className="modal-title" id="exampleModalLabel">Payment</h5>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
