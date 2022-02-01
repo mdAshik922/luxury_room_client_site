c6e23e64fb4713bfe81f2943fd459cea289ce819
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-01 08:35:55 +0600

    commit

--- src/Component/Pages/Dashboard/Order/Order.js
+++ src/Component/Pages/Dashboard/Order/Order.js
@@ -90,37 +90,6 @@ const Order = () => {
                       { order.status } </button>
                   </td>
                   <td>
-                      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Pay</button>
-                        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Payment</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        <form>
-          <div className="mb-3">
-            <h3>${order.price}</h3>
-            <img width="100px" src={ order.img } alt="product" />
-            <label htmlFor="recipient-name" className="col-form-label">Recipient:</label>
-            <input type="text" className="form-control" id="recipient-name"/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="message-text" className="col-form-label">Message:</label>
-            <textarea className="form-control" id="message-text"></textarea>
-          </div>
-        </form>
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Confirm</button>
-      </div>
-    </div>
-  </div>
-</div>
-                    </td>
-                  <td>
                     <Button  variant="outline-danger"
                       className="p-1 ml-3 mb-0"
                       onClick={() => deletion(order._id)}>
